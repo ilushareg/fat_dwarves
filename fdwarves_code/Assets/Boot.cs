db5b4a2a389c66fbcddda5fcaad1d7d7818a4c6e
ilushareg <ilushareg@gmail.com>
2017-12-08 09:25:36 +0300

    just a clean project

---
+++ fdwarves_code/Assets/Boot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void Awake ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void StartServer()
+    {
+
+    }
+    void StartClient()
+    {
+
+    }
+}
